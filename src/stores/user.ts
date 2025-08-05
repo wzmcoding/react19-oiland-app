85116eb9336354650127144efad34f0e1d11dee0
wzm <2791063637@qq.com>
2025-08-05 17:23:20 +0800

    feat: 更新个人资料视图，集成表单组件并优化用户交互

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -12,11 +12,23 @@ type Actions = {
     reset: () => void
 }
 
-interface UserInfo {
+export interface UserInfo {
     avatar: string
+    username: string
     nickname: string
     uid: string
     phone: string
+    level: string
+    lastActivityTime: string // 最近活跃时间
+    newUser: boolean // 是否新用户
+    gradeInfos?: GradeInfo[]
+}
+
+export interface GradeInfo {
+    gradeId: string // 班级ID
+    gradeName: string // 班级名称
+    gradeNumber: string // 班级码
+    joined: boolean // 是否已加入班级
 }
 
 export const useUserStore = create<State & Actions>()(persist((set) => ({
